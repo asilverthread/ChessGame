896ed4f9cd8e6d34db8de433b03573cd666840ba
Zach Cortese <zachary@cortesecrew.com>
2022-06-05 11:09:26 -0400

    started adding event listener and whether piece is selected

--- src/PawnPiece.java
+++ src/PawnPiece.java
@@ -1,14 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PawnPiece implements Piece{
-    Color color = null;
-    final String pieceType = "Pawn";
-    JLabel piece;
-    int[] currentPosition = new int[2];
+    private Color color = null;
+    private final String pieceType = "Pawn";
+    private JLabel piece;
+    private int[] currentPosition = new int[2];
+    private boolean selected;
 
     public PawnPiece() {
         piece = new JLabel(pieceType);
+        piece.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                setSelected();
+            }
+        });
     }
 
     public PawnPiece(String team) {
@@ -23,7 +31,20 @@ public class PawnPiece implements Piece{
 
     @Override
     public boolean isSelected() {
-        return false;
+        return selected;
+    }
+
+    private void updatePiece(){
+        if (selected) {
+            piece.setForeground(Color.YELLOW);
+        } else {
+            piece.setForeground(color);
+        }
+    }
+
+    public void setSelected() {
+        selected = !selected;
+        updatePiece();
     }
 
     @Override
@@ -50,5 +71,6 @@ public class PawnPiece implements Piece{
     public void setLabel() {
 
     }
+
 
 }
